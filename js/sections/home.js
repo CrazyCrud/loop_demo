ce1c1c47572ef9b81acfac2a89f42a89bbb04bb3
CrazyCrud <constantin.lehenmeier@stud.uni-regensburg.de>
2016-02-01 23:52:12 +0100

    Implemented nav and team images

--- js/sections/home.js
+++ js/sections/home.js
@@ -1,12 +1,20 @@
 var App = (function(){
 	var elements = {
+		index: $(".index"),
+		mainNav: $("#main-nav"),
+		menuToggle: $("#mobile-menu-toggle"),
 		news: $("#news-events"),
 		tabs: $("#tabs"),
 		tabNews: $("#tab-news"),
-		tabEvents: $("#tab-events")
+		tabEvents: $("#tab-events"),
+		teamImages: $(".team-images")
 	};
 	var init = function(){
 		console.log("App.init()");
+
+		elements.menuToggle.click(function(event) {
+			elements.mainNav.toggleClass('toggle-nav');
+		});
 
 		elements.tabNews.click(function(event) {
 			elements.tabs.find('.tab-active').removeClass('tab-active');
@@ -20,6 +28,17 @@ var App = (function(){
 			$(this).addClass('tab-active');
 			elements.news.attr('class', '');
 			elements.news.addClass('events-active');
+		});
+
+
+
+		loadTeamImages();
+	};
+
+	var loadTeamImages = function(){
+		elements.teamImages.children('.team-image').each(function(index, el) {
+			var imgLink = $(this).attr('data-image-link');
+			$(this).css('background-image', 'url(' + imgLink + ')');
 		});
 	};
 	return{
